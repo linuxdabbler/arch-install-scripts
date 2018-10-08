2e5698ce92934a7e86d6b4b28b7c5ae1116c7396
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-10-08 00:54:37 -0500

    Create 105-email.sh

---
+++ 105-email.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#################################
+#       Email
+#################################
+
+sudo pacman -S thunderbird --noconfirm
+
+sudo pacman -S neomutt --noconfirm
+
+exit
+
+
