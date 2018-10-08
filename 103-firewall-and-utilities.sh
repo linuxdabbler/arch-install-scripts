edc8071cd4a7b0ea9e96167ceca80da0ed3721f8
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-10-08 00:53:20 -0500

    Create 103-firewall-and-utilities.sh

---
+++ 103-firewall-and-utilities.sh
@@ -0,0 +1,18 @@
+
+#!/bin/sh
+
+############################################
+#       Firewall & Utilities
+############################################
+
+sudo pacman -S gufw --noconfirm
+
+sudo pacman -S gparted --noconfirm
+
+sudo pacman -S bleachbit --noconfirm
+
+exit
+
+
+
+
