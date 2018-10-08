439b9293604cb0d3998363f6b927ae5c7c7ac25e
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-10-08 00:52:18 -0500

    Create 102-downloaders.sh

---
+++ 102-downloaders.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+################################################
+#       Downloaders and Stuff
+################################################
+
+sudo pacman -S transmission-gtk --noconfirm
+
+sudo pacman -S git --noconfirm
+
+sudo pacman -S wget --noconfirm
+
+exit
+
