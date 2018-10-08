0c9d1cc3cba36a93c634391b56d674a4332f4730
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-10-08 00:54:02 -0500

    Create 104-image-editing.sh

---
+++ 104-image-editing.sh
@@ -0,0 +1,20 @@
+
+#!/bin/sh
+
+############################################
+#       Image Editing Software
+############################################
+
+sudo pacman -S gimp --noconfirm
+
+sudo pacman -S inkscape --noconfirm
+
+sudo pacman -S krita --noconfirm
+
+
+
+exit
+
+
+
+
