05df4bdfce53dc9d89b3c01cb33205748505489b
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-10-08 00:55:16 -0500

    Create 106-web-browsers.sh

---
+++ 106-web-browsers.sh
@@ -0,0 +1,17 @@
+
+#!/bin/sh
+
+################################
+#       Web Browswers
+################################
+
+sudo pacman -S firefox --noconfirm
+
+sudo pacman -S chromium --noconfirm
+
+yaourt -S google-chrome --noconfirm
+
+exit
+
+
+
