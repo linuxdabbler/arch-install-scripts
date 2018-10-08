4906f68f3735db87941ec8a72cdc0c755d8b1c81
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-10-08 01:01:25 -0500

    Create 111-desktop.sh

---
+++ 111-desktop.sh
@@ -0,0 +1,35 @@
+
+#!/bin/sh
+
+##############################
+#       Desktop
+##############################
+
+sudo pacman -S ristretto --noconfirm
+
+sudo pacman -S virtualbox --noconfirm
+
+sudo pacman -S compton --noconfirm
+
+sudo pacman -S thunar thunar-archive-plugin thunar-shares-plugin thunar-media-tags-plugin thunar-volman --noconfirm
+
+sudo pacman -S ffmpegthumbnailer --noconfirm
+
+sudo pacman -S cups cups-filters cups-pdf cups-pk-helper --noconfirm
+
+sudo pacman -S system-config-printer --noconfirm
+
+sudo pacman -S varitey --noconfirm
+
+sudo pacman -S nitrogen --noconfirm
+
+sudo pacman -S lxappearance --noconfirm
+
+sudo pacman -S dmenu --noconfirm
+
+sudo pacman -S rofi --noconfirm
+
+sudo pacman -S papirus-icon-theme --noconfirm
+
+sudo pacman -S ttf-font-logos --noconfirm
+
