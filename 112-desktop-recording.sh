75869d22c77159ad9eb204ce2eb39332766006ce
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-10-08 01:02:23 -0500

    Create 112-desktop-recording.sh

---
+++ 112-desktop-recording.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+####################################
+#       Desktop Recording
+####################################
+
+sudo pacman -S obs-studio --noconfirm
+
+sudo pacman -S simplescreenrecorder --noconfirm
+
