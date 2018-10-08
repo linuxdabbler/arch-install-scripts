db2ff8fdc23cc59b8d61c4579e6a6508f4236b13
linuxdabbler <42593036+linuxdabbler@users.noreply.github.com>
2018-10-08 00:57:39 -0500

    Create 107-video-editing-converting-viewing.sh

---
+++ 107-video-editing-converting-viewing.sh
@@ -0,0 +1,18 @@
+
+#!/bin/sh
+
+##############################################################
+#       Video Editing, Converting, and Viewing
+##############################################################
+
+sudo pacman -S kdenlive --nocofirm
+
+sudo pacman -S handbrake --noconfirm
+
+sudo pacman -S vlc --noconfirm
+
+sudo pacman -S mpv gnome-mpv --noconfirm
+
+sudo pacman -S audacity --noconfirm
+
+sudo pacman -S libav-tools --noconfirm
