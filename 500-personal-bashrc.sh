0484c3a7b9f2ffcb4211e426580081658d9b139b
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2019-02-01 05:10:18 -0600

    Update 500-personal-bashrc.sh

--- 500-personal-bashrc.sh
+++ 500-personal-bashrc.sh
@@ -4,42 +4,38 @@
 #       Things added to the end of my .bashrc file for appearance
 ############################################################################################
 
-# change working directory to home folder
-
-cd 
-
 # backup the bashrc just to be safe
 
-cp .bashrc .bashrc.bak
+cp ~/.bashrc ~/.bashrc.bak
 
 # bashrc customization
 
-echo "figlet $USER | lolcat" >> .bashrc
+echo "figlet $USER | lolcat" >> ~/.bashrc
 
-echo "neofetch" >> .bashrc
+echo "neofetch" >> ~/.bashrc
 
 # enable Powerline Shell
+echo "powerline-daemon-q" >> ~/.bashrc
+echo "POWERLINE_BASH_CONTINUATION=1" >> ~/.bashrc
+echo "POWERLINE_BASH_SELECT=1" >> ~/.bashrc
+echo ". /usr/share/powerline/bindings/bash/powerline.sh" >> ~/.bashrc
 
-sudo echo "powerline-daemon-q
->POWERLINE_BASH_CONTINUATION=1
->POWERLINE_BASH_SELECT=1
->. /usr/share/powerline/bindings/bash/powerline.sh" >> ~/.bashrc
 
 # setting up aliases
 
-echo "alias search='sudo pacman -Ss'" >> .bashrc
+echo "alias search='sudo pacman -Ss'" >> ~/.bashrc
 
-echo "alias update='sudo pacman -Syu'" >> .bashrc
+echo "alias update='sudo pacman -Syu'" >> ~/.bashrc
 
-echo "alias install='sudo pacman -S'" >> .bashrc
+echo "alias install='sudo pacman -S'" >> ~/.bashrc
 
-echo "alias nasbox='ssh timelord@192.168.0.19'" >> .bashrc
+echo "alias nasbox='ssh timelord@192.168.0.19'" >> ~/.bashrc
 
-echo "alias senable='sudo systemctl enable'" >> .bashrc
+echo "alias senable='sudo systemctl enable'" >> ~/.bashrc
 
-echo "alias sstart='sudo systemctl start'" >> .bashrc
+echo "alias sstart='sudo systemctl start'" >> ~/.bashrc
 
-echo "alias srestart='sudo systemctl restart'" >> .bashrc
+echo "alias srestart='sudo systemctl restart'" >> ~/.bashrc
 
 
 # restart bash
